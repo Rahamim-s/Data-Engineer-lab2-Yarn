c922f99efaecfffad54df510a8d04a490e65865f
Rahamim-s <rahamime.sitbon@efrei.net>
2023-07-17 17:30:44 +0200

    Add files via upload

--- hadoop-examples-mapreduce-main/src/main/java/com/opstty/AppDriver.java
+++ hadoop-examples-mapreduce-main/src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,6 @@
 package com.opstty;
 
-import com.opstty.job.WordCount;
+import com.opstty.job.*;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,6 +11,15 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("labcountQuestion1", Question1.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("labcountQuestion2", Question2.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("labcountQuestion3", Question3.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("labcountQuestion4", Question4.class,
+                    "A map/reduce program that counts the words in the input files.");
+
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
